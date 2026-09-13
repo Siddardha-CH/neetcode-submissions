0227746192b5fbad2043c6736bcc49f794b32669
Siddardha_CH <siddhusiddhu1712@gmail.com>
2026-09-13 20:21:35 +0530

    Add: count-good-nodes-in-binary-tree - submission-0

---
+++ Data Structures & Algorithms/count-good-nodes-in-binary-tree/submission-0.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public int goodNodes(TreeNode root) {
+        int ans = dfs(root, root.val);
+        return ans;
+    }
+    public int dfs (TreeNode root, int max) {
+        if (root == null)
+            return 0;
+        int k = 0;
+        if (root.val >= max)
+            k = 1;
+        max = Math.max(max, root.val);
+        k += dfs(root.left, max);
+        k += dfs(root.right, max);
+        return k;
+    }
+}
