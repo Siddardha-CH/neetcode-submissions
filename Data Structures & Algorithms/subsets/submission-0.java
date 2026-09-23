62defbeb084a43f2151bccca04cc466266b64946
Siddardha_CH <siddhusiddhu1712@gmail.com>
2026-09-23 11:55:52 +0530

    Add: subsets - submission-0

---
+++ Data Structures & Algorithms/subsets/submission-0.java
@@ -0,0 +1,15 @@
+class Solution {
+    List<List<Integer>> ans = new ArrayList<>();
+    public List<List<Integer>> subsets(int[] nums) {
+        func(nums, 0, new ArrayList<>());
+        return ans;
+    }
+    public void func(int[] nums, int i, ArrayList<Integer> k) {
+        ans.add(new ArrayList<>(k));
+        for (int j = i; j < nums.length; j++) {
+            k.add(nums[j]);
+            func(nums, j + 1, k);
+            k.remove(k.size() - 1);
+        }
+    }
+}
