7d6ce708d99c0c03d9c9b4372c92a29b6ddcac85
Siddardha_CH <siddhusiddhu1712@gmail.com>
2026-09-24 16:09:26 +0530

    Add: combination-target-sum - submission-1

---
+++ Data Structures & Algorithms/combination-target-sum/submission-1.java
@@ -0,0 +1,20 @@
+class Solution {
+    List<List<Integer>> ans = new ArrayList<>();
+    public List<List<Integer>> combinationSum(int[] nums, int target) {
+        func(nums, target, 0, new ArrayList<>());
+        return ans;
+    }
+    public void func (int[] nums, int t, int i, ArrayList<Integer> k) {
+        if (i == nums.length) {
+            if (t == 0)
+                ans.add(new ArrayList<>(k));
+            return;
+        }
+        if (nums[i] <= t) {
+            k.add(nums[i]);
+            func(nums, t - nums[i], i, k);
+            k.remove(k.size() - 1);
+        }
+        func(nums, t, i + 1, k);
+    }
+}
